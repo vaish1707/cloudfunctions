5aa7d482db12194c38547a61d644c78815c74c45
ganny26 <selvaganeshrajam@gmail.com>
2020-10-10 14:09:47 +0530

    Imagekit

---
+++ functions/controllers/index.js
@@ -0,0 +1,41 @@
+const ImageKit = require('imagekit');
+const imagekit = new ImageKit({
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+  urlEndpoint: process.env.URL_END_POINT,
+});
+export function uploadToImageKit(req, res) {
+  let base64data = [];
+  if (req.busboy) {
+    req.busboy
+      .on('file', function (fieldname, file, filename, encoding, mimetype) {
+        console.log('File: ' + filename + ', mimetype: ' + mimetype);
+        let buffer = '';
+        file.setEncoding('base64');
+        file
+          .on('data', function (data) {
+            buffer += data;
+          })
+          .on('end', function () {
+            base64data.push(buffer);
+          });
+      })
+      .on('finish', function () {
+        console.log('data', base64data);
+        imagekit.upload(
+          {
+            file: `data:image/png;base64,${base64data}`,
+            fileName: 'affirmation-image' + Date.now() + '.jpg',
+          },
+          function (error, result) {
+            if (error) {
+              console.log('err', error.message);
+              res.send(error);
+            }
+            console.log('success', result);
+            res.send(result);
+          }
+        );
+      });
+  }
+}
--- functions/index.js
+++ functions/index.js
@@ -10,51 +10,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(busboy({ immediate: true }));
-
-const imagekit = new ImageKit({
-  publicKey: process.env.PUBLIC_KEY,
-  privateKey: process.env.PRIVATE_KEY,
-  urlEndpoint: process.env.URL_END_POINT,
-});
+const controller = require('./controllers');
 
 app.get('/time', (req, res) => {
   res.send(`${Date.now()}`);
 });
 
-app.post('/transform', (req, res) => {
-  let base64data = [];
-  if (req.busboy) {
-    req.busboy
-      .on('file', function (fieldname, file, filename, encoding, mimetype) {
-        console.log('File: ' + filename + ', mimetype: ' + mimetype);
-        let buffer = '';
-        file.setEncoding('base64');
-        file
-          .on('data', function (data) {
-            buffer += data;
-          })
-          .on('end', function () {
-            base64data.push(buffer);
-          });
-      })
-      .on('finish', function () {
-        console.log('data', base64data);
-        imagekit.upload(
-          {
-            file: `data:image/png;base64,${base64data}`,
-            fileName: 'affirmation-image' + Date.now() + '.jpg',
-          },
-          function (error, result) {
-            if (error) {
-              console.log('err', error.message);
-              res.send(error);
-            }
-            console.log('success', result);
-            res.send(result);
-          }
-        );
-      });
-  }
-});
+app.post('/transform', controller.uploadToImageKit);
 
 exports.app = functions.runWith({ timeoutSeconds: 120 }).https.onRequest(app);
